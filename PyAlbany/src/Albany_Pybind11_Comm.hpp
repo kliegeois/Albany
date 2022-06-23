5721ec820d9d2ace43831393032fd369a09418b5
kliegeois <kimliegeois@ymail.com>
2022-06-23 08:16:23 -0600

    tmp

---
+++ PyAlbany/src/Albany_Pybind11_Comm.hpp
@@ -0,0 +1,72 @@
+#include <mpi.h>
+#include <mpi4py/mpi4py.h>
+
+using Teuchos_Comm_PyAlbany = Teuchos::Comm<int>;
+using RCP_Teuchos_Comm_PyAlbany = Teuchos::RCP<const Teuchos_Comm_PyAlbany >;
+
+struct mpi4py_comm {
+  mpi4py_comm() = default;
+  mpi4py_comm(MPI_Comm value) : value(value) {}
+  operator MPI_Comm () { return value; }
+
+  MPI_Comm value;
+};
+
+namespace pybind11 { namespace detail {
+  template <> struct type_caster<mpi4py_comm> {
+    public:
+      PYBIND11_TYPE_CASTER(mpi4py_comm, _("mpi4py_comm"));
+
+      // Python -> C++
+      bool load(handle src, bool) {
+        if (import_mpi4py() < 0) {
+          throw pybind11::error_already_set();
+        }
+
+        PyObject *py_src = src.ptr();
+
+        // Check that we have been passed an mpi4py communicator
+        if (PyObject_TypeCheck(py_src, &PyMPIComm_Type)) {
+          // Convert to regular MPI communicator
+          value.value = *PyMPIComm_Get(py_src);
+          return !PyErr_Occurred();
+        }
+
+        return false;
+      }
+
+      // C++ -> Python
+      static handle cast(mpi4py_comm src,
+                         return_value_policy /* policy */,
+                         handle /* parent */)
+      {
+        // Create an mpi4py handle
+        return PyMPIComm_New(src.value);
+      }
+  };
+}} // namespace pybind11::detail
+
+RCP_Teuchos_Comm_PyAlbany
+getDefaultComm (std::vector<std::string> stdvec_args) {
+
+    int argc = (int)stdvec_args.size();
+    char **argv = new char*[argc+1];
+    for (int i = 0; i < argc; ++i) {
+        argv[i] = (char*)stdvec_args[i].data();
+    }
+    argv[argc] = nullptr;
+
+    MPI_Init(&argc, &argv);
+
+    return Teuchos::DefaultComm<int>::getComm();
+}
+
+RCP_Teuchos_Comm_PyAlbany
+getTeuchosComm (mpi4py_comm comm) {
+    return Teuchos::rcp<const Teuchos_Comm_PyAlbany>(new Teuchos::MpiComm< int >
+      (Teuchos::opaqueWrapper(comm.value)));
+}
+
+void finalize() {
+    MPI_Finalize();
+}
---
+++ PyAlbany/src/Albany_Pybind11_ParallelEnv.hpp
@@ -0,0 +1,23 @@
+#include "Kokkos_Core.hpp"
+
+class PyParallelEnv
+{
+public:
+    RCP_Teuchos_Comm_PyAlbany comm;
+    const int num_threads, num_numa, device_id;
+
+    PyParallelEnv(RCP_Teuchos_Comm_PyAlbany _comm, int _num_threads = -1, int _num_numa = -1, int _device_id = -1) : comm(_comm), num_threads(_num_threads), num_numa(_num_numa), device_id(_device_id)
+    {
+        Kokkos::InitArguments args;
+        args.num_threads = this->num_threads;
+        args.num_numa = this->num_numa;
+        args.device_id = this->device_id;
+
+        Kokkos::initialize(args);
+    }
+    ~PyParallelEnv()
+    {
+        Kokkos::finalize_all();
+        std::cout << "~PyParallelEnv()\n";
+    }
+};
